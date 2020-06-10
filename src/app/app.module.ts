5a6b381236768373d39fbdc1f1b0b36b7fbb5b3f
anshiyatreaz <anshiya33@gmail.com>
2020-06-10 12:41:33 +0530

    4 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule],
   declarations: [ AppComponent, HelloComponent, SignUpComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,33 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
-  
-import { ConfirmedValidator } from '../confirmedvalidator';
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from '../must-match-validator';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+    registerForm: FormGroup;
+    submitted = false;
 
-   form: FormGroup = new FormGroup({});
-  
-  constructor(private fb: FormBuilder) {
-  
-    this.form = fb.group({
-      password: ['', [Validators.required]],
-      confirm_password: ['', [Validators.required]]
-    }, { 
-      validator: ConfirmedValidator('password', 'confirm_password')
-    })
-  }
-    
-  get f(){
-    return this.form.controls;
-  }
-   
-  submit(){
-    console.log(this.form.value);
-  }
+    constructor(private formBuilder: FormBuilder) { }
 
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    }
 }
